deb8310de898814016518f2ba50923d7b7812925
mingle08 <mingle08@163.com>
2017-12-17 19:21:45 +0800

    增加注释
    关于获取数据库的连接，核心编程（第3版）用的是MySQLdb.connect()，我尝试了，并不成功。后来在网上查了，有人用mysql.connector.connect()，尝试了这个，成功了。

--- pythonDemo/selectDemo.py
+++ pythonDemo/selectDemo.py
@@ -1,10 +1,12 @@
-﻿import mysql.connector
+import mysql.connector
 
+# 安装的python版本是3.4，数据库驱动是： mysql-connector-python-2.1.7-py3.4-windows-x86-64bit.msi
+# python核心编程（第3版）205页，获取连接用的是MySQLdb，尝试了不成功，换成mysql.connector就成功了。
 conn = mysql.connector.connect(
     host='127.0.0.1',
     port=3306,
     user='root',
-    password='hu820826',
+    password='h******',
     database='imooc'
     )
 cursor = conn.cursor()
