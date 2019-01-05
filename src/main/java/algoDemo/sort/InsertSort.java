7213eb02826a67a62503bd1484e9463cbc56c91e
mingle08 <mingle08@163.com>
2019-01-06 01:27:14 +0800

    插入算法

--- src/main/java/algoDemo/sort/InsertSort.java
+++ src/main/java/algoDemo/sort/InsertSort.java
@@ -52,4 +52,25 @@ public class InsertSort {
         }  
 	}
 
+	public static void swap(int []arr,int a,int b){
+		arr[a] = arr[a]+arr[b];
+		arr[b] = arr[a]-arr[b];
+		arr[a] = arr[a]-arr[b];
+	}
+
+	/**
+	 * 摘自博客：
+	 * http://www.cnblogs.com/chengxiao/p/6103002.html
+	 * @param arr
+	 */
+	public static void insertionSort(int[] arr) {
+		for (int i = 1; i < arr.length; i++) {
+			int j = i;
+			while (j > 0 && arr[j] < arr[j - 1]) {
+				swap(arr,j,j-1);
+				j--;
+			}
+		}
+	}
+
 }
